7119f4e4ad596b4cdc565d327bd865e1a7f5ce59
Joao Martins Costa <jpmcosta@gmail.com>
2014-03-21 00:07:05 +0000

    AudioPlayerSeekBar in sync with MediaPlayer

--- app/src/main/java/com/audiomanager/widget/AudioPlayerSeekBar.java
+++ app/src/main/java/com/audiomanager/widget/AudioPlayerSeekBar.java
@@ -2,24 +2,42 @@ package com.audiomanager.widget;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.widget.SeekBar;
 
-public class AudioPlayerSeekBar extends SeekBar {
+public class AudioPlayerSeekBar extends SeekBar implements
+		SeekBar.OnSeekBarChangeListener,
+		MediaPlayer.OnBufferingUpdateListener,
+		MediaPlayer.OnCompletionListener {
 	private final static int UPDATE_INTERVAL = 200;
 
 	private MediaPlayer mMediaPlayer;
 
+	private boolean mIsPlaying = false;
+	private boolean mIsCompleted = false;
+
+	private int mBufferProgress;
+
+	private ProgressUpdater mProgressUpdate = new ProgressUpdater();
+
 	public AudioPlayerSeekBar(Context context) {
 		super(context);
+		init();
 	}
 
 	public AudioPlayerSeekBar(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 
 	public AudioPlayerSeekBar(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		init();
+	}
+
+	private void init() {
+		setOnSeekBarChangeListener(this);
 	}
 
 	@Override
@@ -39,16 +57,85 @@ public class AudioPlayerSeekBar extends SeekBar {
 		mMediaPlayer = mediaPlayer;
 
 		if(mMediaPlayer != null) {
-			postDelayed(new Cena(), UPDATE_INTERVAL);
+			setMax(mediaPlayer.getDuration());
+			mMediaPlayer.setOnBufferingUpdateListener(this);
+			mMediaPlayer.setOnCompletionListener(this);
+
+			startPlay();
 		}
 	}
 
-	private class Cena implements Runnable {
+	@Override
+	public void onBufferingUpdate(MediaPlayer mp, int percent) {
+		mBufferProgress = (int)((percent / 100.0f) * mp.getDuration()) - 10;
+
+		if(!mIsPlaying)
+			startPlay();
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		stopPlay();
+	}
+
+	public void startPlay() {
+		mIsCompleted = false;
+		mIsPlaying = mMediaPlayer.isPlaying();
+
+		if(mIsPlaying) {
+			stopProgressUpdater();
+			post(mProgressUpdate);
+		}
+	}
+
+	public void stopPlay() {
+		mIsCompleted = true;
+		mIsPlaying = false;
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		// Do nothing.
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		if(mIsPlaying)
+			stopProgressUpdater();
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		if(mMediaPlayer != null) {
+			mMediaPlayer.seekTo(getProgress());
+
+			if(mIsPlaying)
+				startProgressUpdater();
+		}
+	}
+
+	protected void startProgressUpdater() {
+		post(mProgressUpdate);
+	}
+
+	protected void stopProgressUpdater() {
+		final Handler handler = getHandler();
+		if(handler != null)
+			handler.removeCallbacks(mProgressUpdate);
+	}
+
+	private class ProgressUpdater implements Runnable {
 		@Override
 		public void run() {
 			if(mMediaPlayer != null) {
-				setProgress(getProgress() + 1);
-				postDelayed(this, UPDATE_INTERVAL);
+				if(mMediaPlayer.isPlaying()) {
+					setProgress(mMediaPlayer.getCurrentPosition());
+					setSecondaryProgress(mBufferProgress);
+					postDelayed(this, UPDATE_INTERVAL);
+				}
+				else if(mIsCompleted) {
+					setProgress(getMax());
+				}
 			}
 		}
 	}
