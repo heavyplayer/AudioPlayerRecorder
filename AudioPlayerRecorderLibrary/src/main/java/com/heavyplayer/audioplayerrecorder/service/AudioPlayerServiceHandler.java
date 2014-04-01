b00538c21add0b3bfa29cbcfe32bbab4ea0ba184
Joao Martins Costa <jpmcosta@gmail.com>
2014-04-01 17:36:39 +0100

    Easier way to use player service in activities

---
+++ AudioPlayerRecorderLibrary/src/main/java/com/heavyplayer/audioplayerrecorder/service/AudioPlayerServiceHandler.java
@@ -0,0 +1,91 @@
+package com.heavyplayer.audioplayerrecorder.service;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.DisplayMetrics;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.heavyplayer.audioplayerrecorder.R;
+
+public class AudioPlayerServiceHandler {
+	private AudioPlayerService.LocalBinder mAudioPlayerBinder;
+	private AudioPlayerServiceConnection mServiceConnection = new AudioPlayerServiceConnection();
+
+	private Activity mActivity;
+
+	private boolean mIsPortrait;
+
+	private ListView mListView;
+
+	public AudioPlayerServiceHandler(Activity activity) {
+		mActivity = activity;
+	}
+
+	public void onActivityStart() {
+		mIsPortrait = isPortrait();
+
+		mActivity.startService(new Intent(mActivity, AudioPlayerService.class));
+	}
+
+	public void onActivityResume() {
+		bindService();
+	}
+
+	public void onActivityPause() {
+		unbindService();
+
+		if(mIsPortrait == isPortrait())
+			mActivity.stopService(new Intent(mActivity, AudioPlayerService.class));
+	}
+
+	protected void bindService() {
+		mActivity.bindService(
+				new Intent(mActivity, AudioPlayerService.class),
+				mServiceConnection,
+				Context.BIND_AUTO_CREATE);
+	}
+
+	protected void unbindService() {
+		if(mAudioPlayerBinder != null) {
+			mAudioPlayerBinder = null;
+			mActivity.unbindService(mServiceConnection);
+		}
+	}
+
+	protected boolean isPortrait() {
+		DisplayMetrics displaymetrics = new DisplayMetrics();
+		mActivity.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+		return displaymetrics.heightPixels > displaymetrics.widthPixels;
+	}
+
+	public void setListView(ListView listView) {
+		mListView = listView;
+	}
+
+	public AudioPlayerService.LocalBinder getAudioPlayerBinder() {
+		return mAudioPlayerBinder;
+	}
+
+	private class AudioPlayerServiceConnection implements ServiceConnection {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			mAudioPlayerBinder = (AudioPlayerService.LocalBinder)service;
+
+			if(mListView != null)
+				mListView.invalidateViews();
+
+			Toast.makeText(mActivity, R.string.local_service_connected, Toast.LENGTH_SHORT).show();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mAudioPlayerBinder = null;
+
+			Toast.makeText(mActivity, R.string.local_service_disconnected, Toast.LENGTH_SHORT).show();
+		}
+	}
+}
--- AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/PlayerActivity.java
+++ AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/PlayerActivity.java
@@ -1,29 +1,21 @@
 package com.heavyplayer.audioplayerrecorder.sample.activity;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
-import android.os.IBinder;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
-import android.widget.Toast;
 import com.heavyplayer.audioplayerrecorder.sample.R;
 import com.heavyplayer.audioplayerrecorder.sample.obj.Item;
-import com.heavyplayer.audioplayerrecorder.service.AudioPlayerService;
+import com.heavyplayer.audioplayerrecorder.service.AudioPlayerServiceHandler;
 import com.heavyplayer.audioplayerrecorder.widget.AudioPlayerLayout;
 
 import java.io.File;
 
 public class PlayerActivity extends RecorderActivity {
-	private AudioPlayerService.LocalBinder mAudioPlayerBinder;
-	private AudioPlayerServiceConnection mServiceConnection = new AudioPlayerServiceConnection();
-
-	private boolean mIsPortrait;
+	protected AudioPlayerServiceHandler mAudioPlayerServiceHandler;
 
 	@Override
 	protected ListAdapter onCreateAdapter(Context context, Item[] objects) {
@@ -31,77 +23,28 @@ public class PlayerActivity extends RecorderActivity {
 	}
 
 	@Override
-	protected void onStart() {
-		super.onStart();
-		mIsPortrait = isPortrait();
-
-		startService(new Intent(this, AudioPlayerService.class));
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mAudioPlayerServiceHandler = new AudioPlayerServiceHandler(this);
+		mAudioPlayerServiceHandler.setListView(mListView);
 	}
 
-	protected boolean isPortrait() {
-		DisplayMetrics displaymetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-		return displaymetrics.heightPixels > displaymetrics.widthPixels;
+	@Override
+	protected void onStart() {
+		super.onStart();
+		mAudioPlayerServiceHandler.onActivityStart();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-		bindService();
+		mAudioPlayerServiceHandler.onActivityResume();
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-
-		unbindService();
-
-		if(mIsPortrait == isPortrait())
-			stopService(new Intent(this, AudioPlayerService.class));
-	}
-
-	protected void bindService() {
-		bindService(
-				new Intent(this, AudioPlayerService.class),
-				mServiceConnection,
-				Context.BIND_AUTO_CREATE);
-	}
-
-	protected void unbindService() {
-		if(mAudioPlayerBinder != null) {
-			mAudioPlayerBinder = null;
-			// Detach our existing connection.
-			unbindService(mServiceConnection);
-		}
-	}
-
-	private class AudioPlayerServiceConnection implements ServiceConnection {
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			// This is called when the connection with the service has been
-			// established, giving us the service object we can use to
-			// interact with the service.  Because we have bound to a explicit
-			// service that we know is running in our own process, we can
-			// cast its IBinder to a concrete class and directly access it.
-			mAudioPlayerBinder = (AudioPlayerService.LocalBinder)service;
-
-			mListView.invalidateViews();
-
-			// Tell the user about this for our demo.
-			Toast.makeText(PlayerActivity.this, R.string.local_service_connected, Toast.LENGTH_SHORT).show();
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			// This is called when the connection with the service has been
-			// unexpectedly disconnected -- that is, its process crashed.
-			// Because it is running in our same process, we should never
-			// see this happen.
-			mAudioPlayerBinder = null;
-
-			Toast.makeText(PlayerActivity.this, R.string.local_service_disconnected, Toast.LENGTH_SHORT).show();
-		}
+		mAudioPlayerServiceHandler.onActivityPause();
 	}
 
 	protected class AudioPlayerItemAdapter extends ItemAdapter {
@@ -113,12 +56,12 @@ public class PlayerActivity extends RecorderActivity {
 		public View getView(int position, View convertView, ViewGroup parent) {
 			final View view = super.getView(position, convertView, parent);
 
-			if(mAudioPlayerBinder != null) {
+			if(mAudioPlayerServiceHandler.getAudioPlayerBinder() != null) {
 				final AudioPlayerLayout audioPlayerLayout =
 						(AudioPlayerLayout)view.findViewById(R.id.item_audio_player);
 				if(audioPlayerLayout != null) {
 					final Item item = getItem(position);
-					mAudioPlayerBinder.register(
+					mAudioPlayerServiceHandler.getAudioPlayerBinder().register(
 							item.getId(),
 							generateExternalStorageFileUri(item.getFileName()),
 							audioPlayerLayout);
