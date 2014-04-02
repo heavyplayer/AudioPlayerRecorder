c3df413bb07aac588e862c1f49b1443ace953377
Joao Martins Costa <jpmcosta@gmail.com>
2014-04-02 17:10:42 +0100

    Gain audio focus when recording audio

--- AudioPlayerRecorderLibrary/src/main/java/com/heavyplayer/audioplayerrecorder/service/AudioRecorderService.java
+++ AudioPlayerRecorderLibrary/src/main/java/com/heavyplayer/audioplayerrecorder/service/AudioRecorderService.java
@@ -3,9 +3,11 @@ package com.heavyplayer.audioplayerrecorder.service;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
@@ -16,15 +18,13 @@ import com.heavyplayer.audioplayerrecorder.widget.AudioRecorderMicrophone;
 import java.util.HashSet;
 import java.util.Set;
 
-public class AudioRecorderService extends Service {
+public class AudioRecorderService extends Service implements AudioManager.OnAudioFocusChangeListener {
 	public static final String TAG = AudioRecorderService.class.getSimpleName();
 
 	private final static int UPDATE_INTERVAL_MS = 100;
 
 	public static final String EXTRA_FILE_URI = "extra_file_uri";
 
-	// This is the object that receives interactions from clients.  See
-	// RemoteService for a more complete example.
 	private final IBinder mBinder = new LocalBinder();
 
 	private Handler mHandler;
@@ -42,6 +42,8 @@ public class AudioRecorderService extends Service {
 
 	@Override
 	public void onCreate() {
+		gainAudioFocus();
+
 		mHandler = new Handler();
 
 		// Tell the user we started.
@@ -90,6 +92,8 @@ public class AudioRecorderService extends Service {
 			}
 		}
 
+		abandonAudioFocus();
+
 		// Tell the user we stopped.
 		Toast.makeText(this, R.string.local_service_stopped, Toast.LENGTH_SHORT).show();
 	}
@@ -99,11 +103,32 @@ public class AudioRecorderService extends Service {
 		return mBinder;
 	}
 
-	/**
-	 * Class for clients to access.  Because we know this service always
-	 * runs in the same process as its clients, we don't need to deal with
-	 * IPC.
-	 */
+	protected void gainAudioFocus() {
+		final int durationHint;
+		if(Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT)
+			durationHint = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT;
+		else
+			// Request audio focus for recording without being disturbed by system sounds.
+			durationHint = AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE;
+
+		final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		audioManager.requestAudioFocus(
+				this,
+				AudioManager.STREAM_MUSIC,
+				durationHint);
+	}
+
+	protected void abandonAudioFocus() {
+		// Abandon audio focus when the recording complete.
+		final AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		audioManager.abandonAudioFocus(this);
+	}
+
+	@Override
+	public void onAudioFocusChange(int focusChange) {
+		// Do nothing.
+	}
+
 	public class LocalBinder extends Binder {
 		public void registerAudioRecorderMicrophone(AudioRecorderMicrophone microphone) {
 			mMicrophones.add(microphone);
