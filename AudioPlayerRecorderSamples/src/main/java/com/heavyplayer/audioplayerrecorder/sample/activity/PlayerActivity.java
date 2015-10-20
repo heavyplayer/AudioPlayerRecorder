24e3a34fbcd7bdd66d4ccaeeac23844aa22887bb
Olga Vshivtseva <olga.vshivtseva@rebelmouse.com>
2015-10-20 00:52:56 -0700

    Rid of storage permission request in sample app

--- AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/PlayerActivity.java
+++ AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/PlayerActivity.java
@@ -1,13 +1,5 @@
 package com.heavyplayer.audioplayerrecorder.sample.activity;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.IBinder;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListAdapter;
 import com.heavyplayer.audioplayerrecorder.sample.R;
 import com.heavyplayer.audioplayerrecorder.sample.obj.Item;
 import com.heavyplayer.audioplayerrecorder.service.AudioPlayerService;
@@ -15,76 +7,100 @@ import com.heavyplayer.audioplayerrecorder.service.manager.AudioPlayerServiceMan
 import com.heavyplayer.audioplayerrecorder.service.manager.ServiceManager;
 import com.heavyplayer.audioplayerrecorder.widget.AudioPlayerLayout;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.Toast;
+
 import java.io.File;
 
 public class PlayerActivity extends RecorderActivity {
-	protected AudioPlayerServiceManager mAudioPlayerServiceManager;
+    protected AudioPlayerServiceManager mAudioPlayerServiceManager;
 
-	@Override
-	protected ListAdapter onCreateAdapter(Context context, Item[] objects) {
-		return new AudioPlayerItemAdapter(context, objects);
-	}
+    @Override
+    protected ListAdapter onCreateAdapter(Context context, Item[] objects) {
+        return new AudioPlayerItemAdapter(context, objects);
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mAudioPlayerServiceManager = new AudioPlayerServiceManager(this);
-		mAudioPlayerServiceManager.setServiceStateListener(new ServiceManager.StateListener() {
-			@Override
-			public void onServiceBind(IBinder binder) {
-				if(mListView != null)
-					mListView.invalidateViews();
-			}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAudioPlayerServiceManager = new AudioPlayerServiceManager(this);
+        mAudioPlayerServiceManager.setServiceStateListener(new ServiceManager.StateListener() {
+            @Override
+            public void onServiceBind(IBinder binder) {
+                if (mListView != null) {
+                    mListView.invalidateViews();
+                }
+            }
 
-			@Override
-			public void onServiceUnbind(IBinder binder) { }
+            @Override
+            public void onServiceUnbind(IBinder binder) {
+            }
 
-			@Override
-			public void onServiceStop() { }
-		});
-	}
+            @Override
+            public void onServiceStop() {
+            }
+        });
+    }
 
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mAudioPlayerServiceManager.onActivityResume();
-	}
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAudioPlayerServiceManager.onActivityResume();
+    }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mAudioPlayerServiceManager.onActivityPause();
-	}
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAudioPlayerServiceManager.onActivityPause();
+    }
 
-	protected class AudioPlayerItemAdapter extends ItemAdapter {
-		public AudioPlayerItemAdapter(Context context, Item[] objects) {
-			super(context, objects);
-		}
+    protected class AudioPlayerItemAdapter extends ItemAdapter {
+        public AudioPlayerItemAdapter(Context context, Item[] objects) {
+            super(context, objects);
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			final View view = super.getView(position, convertView, parent);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final View view = super.getView(position, convertView, parent);
 
-			final AudioPlayerService.LocalBinder binder = mAudioPlayerServiceManager.getBinder();
-			if(binder != null) {
-				final AudioPlayerLayout audioPlayerLayout =
-						(AudioPlayerLayout)view.findViewById(R.id.item_audio_player);
-				if(audioPlayerLayout != null) {
-					final Item item = getItem(position);
-					binder.register(
-							item.getId(),
-							generateExternalStorageFileUri(item.getFileName()),
-							true,
-							audioPlayerLayout);
-				}
-			}
+            final AudioPlayerService.LocalBinder binder = mAudioPlayerServiceManager.getBinder();
+            if (binder != null) {
+                final AudioPlayerLayout audioPlayerLayout =
+                        (AudioPlayerLayout) view.findViewById(R.id.item_audio_player);
+                if (audioPlayerLayout != null) {
+                    final Item item = getItem(position);
+                    Uri uri = generateExternalStorageFileUri(item.getFileName());
+                    if (uri != null) {
+                        binder.register(
+                                item.getId(),
+                                generateExternalStorageFileUri(item.getFileName()),
+                                true,
+                                audioPlayerLayout);
+                    } else {
+                        Toast.makeText(getContext(), R.string.error_storage_not_available, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
 
-			return view;
-		}
-	}
+            return view;
+        }
+    }
 
-	protected Uri generateExternalStorageFileUri(String fileName) {
-		final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
-		return Uri.fromFile(file);
-	}
+    @Nullable
+    protected Uri generateExternalStorageFileUri(String fileName) {
+        final File filesDir = getExternalFilesDir(null);
+        if (filesDir != null) {
+            final File file = new File(filesDir.getAbsolutePath(), fileName);
+            return Uri.fromFile(file);
+        } else {
+            return null;
+        }
+    }
 }
--- AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/RecorderActivity.java
+++ AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/RecorderActivity.java
@@ -11,9 +11,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -126,14 +126,25 @@ public class RecorderActivity extends AppCompatActivity
                 return;
             }
 
-            AudioRecorderFragment.newInstance(generateExternalStorageFileUri(fileName))
-                                 .show(getSupportFragmentManager(), AudioRecorderFragment.TAG);
+            Uri uri = generateExternalStorageFileUri(fileName);
+            if (uri != null) {
+                AudioRecorderFragment.newInstance(uri)
+                                     .show(getSupportFragmentManager(), AudioRecorderFragment.TAG);
+            } else {
+                Toast.makeText(this, R.string.error_storage_not_available, Toast.LENGTH_LONG).show();
+            }
         }
     }
 
+    @Nullable
     protected Uri generateExternalStorageFileUri(String fileName) {
-        final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
-        return Uri.fromFile(file);
+        File filesDir = getExternalFilesDir(null);
+        if (filesDir != null) {
+            final File file = new File(filesDir.getAbsolutePath(), fileName);
+            return Uri.fromFile(file);
+        } else {
+            return null;
+        }
     }
 
     protected Item getSelectedItem() {
