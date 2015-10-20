1f00edf33f649c372547c6063a00086bba9ffc00
Olga Vshivtseva <olga.vshivtseva@rebelmouse.com>
2015-10-19 20:08:53 -0700

    Update gradle configs, fix sample build error

--- AudioPlayerRecorderLibrary/src/main/java/com/heavyplayer/audioplayerrecorder/fragment/AudioRecorderFragment.java
+++ AudioPlayerRecorderLibrary/src/main/java/com/heavyplayer/audioplayerrecorder/fragment/AudioRecorderFragment.java
@@ -1,5 +1,11 @@
 package com.heavyplayer.audioplayerrecorder.fragment;
 
+import com.heavyplayer.audioplayerrecorder.R;
+import com.heavyplayer.audioplayerrecorder.service.AudioRecorderService;
+import com.heavyplayer.audioplayerrecorder.service.manager.AudioRecorderServiceManager;
+import com.heavyplayer.audioplayerrecorder.service.manager.ServiceManager;
+import com.heavyplayer.audioplayerrecorder.widget.AudioRecorderMicrophone;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -11,144 +17,147 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.heavyplayer.audioplayerrecorder.R;
-import com.heavyplayer.audioplayerrecorder.service.AudioRecorderService;
-import com.heavyplayer.audioplayerrecorder.service.manager.AudioRecorderServiceManager;
-import com.heavyplayer.audioplayerrecorder.service.manager.ServiceManager;
-import com.heavyplayer.audioplayerrecorder.widget.AudioRecorderMicrophone;
-
 public class AudioRecorderFragment extends DialogFragment
-		implements View.OnClickListener, ServiceManager.StateListener, AudioRecorderService.AudioRecorderStateListener {
-	private static final String ARG_FILE_URI = "arg_file_uri";
+        implements View.OnClickListener, ServiceManager.StateListener, AudioRecorderService.AudioRecorderStateListener {
+    public static final String TAG = AudioRecorderFragment.class.getSimpleName();
 
-	private AudioRecorderServiceManager mAudioRecorderServiceManager;
+    private static final String ARG_FILE_URI = "arg_file_uri";
 
-	private Uri mFileUri;
+    private AudioRecorderServiceManager mAudioRecorderServiceManager;
 
-	private boolean mStartRecorderOnBind;
+    private Uri mFileUri;
 
-	public static AudioRecorderFragment newInstance(Uri fileUri) {
-		return newInstance(new AudioRecorderFragment(), fileUri);
-	}
-	protected static <T extends AudioRecorderFragment> T newInstance(T fragment, Uri fileUri) {
-		final Bundle args = new Bundle();
-		args.putParcelable(ARG_FILE_URI, fileUri);
+    private boolean mStartRecorderOnBind;
 
-		fragment.setArguments(args);
+    public static AudioRecorderFragment newInstance(Uri fileUri) {
+        return newInstance(new AudioRecorderFragment(), fileUri);
+    }
 
-		return fragment;
-	}
+    protected static <T extends AudioRecorderFragment> T newInstance(T fragment, Uri fileUri) {
+        final Bundle args = new Bundle();
+        args.putParcelable(ARG_FILE_URI, fileUri);
 
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
+        fragment.setArguments(args);
 
-		mAudioRecorderServiceManager = new AudioRecorderServiceManager(activity);
-		mAudioRecorderServiceManager.setServiceStateListener(this);
-	}
+        return fragment;
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
 
-		mStartRecorderOnBind = savedInstanceState == null;
-	}
+        mAudioRecorderServiceManager = new AudioRecorderServiceManager(activity);
+        mAudioRecorderServiceManager.setServiceStateListener(this);
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		mAudioRecorderServiceManager.onFragmentResume();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		mAudioRecorderServiceManager.onFragmentPause();
-	}
+        mStartRecorderOnBind = savedInstanceState == null;
+    }
 
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		final Activity activity = getActivity();
+    @Override
+    public void onResume() {
+        super.onResume();
+        mAudioRecorderServiceManager.onFragmentResume();
+    }
 
-		final View view = LayoutInflater.from(activity).inflate(R.layout.audio_recorder, null);
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAudioRecorderServiceManager.onFragmentPause();
+    }
 
-		return new AlertDialog.Builder(activity)
-				.setTitle(R.string.audio_recorder_recording)
-				.setView(view)
-				.setPositiveButton(R.string.audio_recorder_stop_recording, null)
-				.create();
-	}
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Activity activity = getActivity();
 
-	public Uri getFileUri() {
-		if(mFileUri == null)
-			mFileUri = getArguments().getParcelable(ARG_FILE_URI);
-		return mFileUri;
-	}
+        final View view = LayoutInflater.from(activity).inflate(R.layout.audio_recorder, null);
 
-	@Override
-	final public void onClick(View v) {
-		final AudioRecorderService.LocalBinder binder = mAudioRecorderServiceManager.getBinder();
-		final boolean isRecording = binder != null && binder.isRecording();
-		onMicrophoneClick(v, isRecording);
-	}
+        return new AlertDialog.Builder(activity)
+                .setTitle(R.string.audio_recorder_recording)
+                .setView(view)
+                .setPositiveButton(R.string.audio_recorder_stop_recording, null)
+                .create();
+    }
 
-	protected void onMicrophoneClick(View v, boolean isRecording) {
-		final AudioRecorderService.LocalBinder binder = mAudioRecorderServiceManager.getBinder();
-		if(binder != null) {
-			if(binder.isRecording())
-				binder.stopRecorder();
-			else
-				binder.startRecorder(getFileUri());
-		}
-	}
+    public Uri getFileUri() {
+        if (mFileUri == null) {
+            mFileUri = getArguments().getParcelable(ARG_FILE_URI);
+        }
+        return mFileUri;
+    }
 
-	@Override
-	public void onServiceBind(IBinder binder) {
-		registerOnAudioRecorderService((AudioRecorderService.LocalBinder)binder);
+    @Override
+    final public void onClick(View v) {
+        final AudioRecorderService.LocalBinder binder = mAudioRecorderServiceManager.getBinder();
+        final boolean isRecording = binder != null && binder.isRecording();
+        onMicrophoneClick(v, isRecording);
+    }
 
-		if(mStartRecorderOnBind) {
-			mStartRecorderOnBind = false;
-			((AudioRecorderService.LocalBinder)binder).startRecorder(getFileUri());
-		}
-	}
+    protected void onMicrophoneClick(View v, boolean isRecording) {
+        final AudioRecorderService.LocalBinder binder = mAudioRecorderServiceManager.getBinder();
+        if (binder != null) {
+            if (binder.isRecording()) {
+                binder.stopRecorder();
+            } else {
+                binder.startRecorder(getFileUri());
+            }
+        }
+    }
 
-	@Override
-	public void onServiceUnbind(IBinder binder) { }
+    @Override
+    public void onServiceBind(IBinder binder) {
+        registerOnAudioRecorderService((AudioRecorderService.LocalBinder) binder);
 
-	protected void registerOnAudioRecorderService(AudioRecorderService.LocalBinder binder) {
-		final AudioRecorderMicrophone microphone =
-				(AudioRecorderMicrophone)getDialog().findViewById(android.R.id.input);
-		if(microphone != null)
-			microphone.setOnClickListener(this);
+        if (mStartRecorderOnBind) {
+            mStartRecorderOnBind = false;
+            ((AudioRecorderService.LocalBinder) binder).startRecorder(getFileUri());
+        }
+    }
 
-		binder.register(microphone, this);
-	}
+    @Override
+    public void onServiceUnbind(IBinder binder) {
+    }
 
-	@Override
-	public void onServiceStop() {
-		// Purposely empty.
-	}
+    protected void registerOnAudioRecorderService(AudioRecorderService.LocalBinder binder) {
+        final AudioRecorderMicrophone microphone =
+                (AudioRecorderMicrophone) getDialog().findViewById(android.R.id.input);
+        if (microphone != null) {
+            microphone.setOnClickListener(this);
+        }
 
-	@Override
-	public void onStartRecorder() {
-		Activity activity = getActivity();
-		if(activity != null)
-			activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	}
+        binder.register(microphone, this);
+    }
 
-	@Override
-	public void onStartRecorderFailed(Exception e) {
-		// Purposely empty.
-	}
+    @Override
+    public void onServiceStop() {
+        // Purposely empty.
+    }
 
-	@Override
-	public void onStopRecorder() {
-		Activity activity = getActivity();
-		if(activity != null)
-			activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-	}
+    @Override
+    public void onStartRecorder() {
+        Activity activity = getActivity();
+        if (activity != null) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
 
-	@Override
-	public void onTimeLimitExceeded() {
-		// Purposely empty.
-	}
+    @Override
+    public void onStartRecorderFailed(Exception e) {
+        // Purposely empty.
+    }
+
+    @Override
+    public void onStopRecorder() {
+        Activity activity = getActivity();
+        if (activity != null) {
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+
+    @Override
+    public void onTimeLimitExceeded() {
+        // Purposely empty.
+    }
 }
--- AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/RecorderActivity.java
+++ AudioPlayerRecorderSamples/src/main/java/com/heavyplayer/audioplayerrecorder/sample/activity/RecorderActivity.java
@@ -1,5 +1,10 @@
 package com.heavyplayer.audioplayerrecorder.sample.activity;
 
+import com.heavyplayer.audioplayerrecorder.fragment.AudioRecorderFragment;
+import com.heavyplayer.audioplayerrecorder.sample.R;
+import com.heavyplayer.audioplayerrecorder.sample.obj.Item;
+import com.heavyplayer.audioplayerrecorder.utils.AudioUtils;
+
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -14,65 +19,61 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.heavyplayer.audioplayerrecorder.fragment.AudioRecorderFragment;
-import com.heavyplayer.audioplayerrecorder.sample.R;
-import com.heavyplayer.audioplayerrecorder.sample.obj.Item;
-import com.heavyplayer.audioplayerrecorder.utils.AudioUtils;
 
 import java.io.File;
 
 public class RecorderActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
-	private static Item[] sItems = {
-			new Item(0),
-			new Item(1),
-			new Item(2),
-			new Item(3),
-			new Item(4),
-			new Item(5),
-			new Item(6),
-			new Item(7),
-			new Item(8),
-			new Item(9),
-			new Item(11),
-			new Item(12),
-			new Item(13),
-			new Item(14),
-	};
+    private static Item[] sItems = {
+            new Item(0),
+            new Item(1),
+            new Item(2),
+            new Item(3),
+            new Item(4),
+            new Item(5),
+            new Item(6),
+            new Item(7),
+            new Item(8),
+            new Item(9),
+            new Item(11),
+            new Item(12),
+            new Item(13),
+            new Item(14),
+    };
 
-	protected TextView mTitleView;
-	protected ListView mListView;
+    protected TextView mTitleView;
+    protected ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-	    mTitleView = (TextView)findViewById(android.R.id.title);
+        mTitleView = (TextView) findViewById(android.R.id.title);
 
-	    mListView = (ListView)findViewById(android.R.id.list);
-	    mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-	    mListView.setAdapter(onCreateAdapter(this, sItems));
-	    mListView.setOnItemClickListener(this);
-	    // Select first item.
-	    mListView.performItemClick(mListView.getChildAt(0), 0, mListView.getItemIdAtPosition(0));
+        mListView = (ListView) findViewById(android.R.id.list);
+        mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        mListView.setAdapter(onCreateAdapter(this, sItems));
+        mListView.setOnItemClickListener(this);
+        // Select first item.
+        mListView.performItemClick(mListView.getChildAt(0), 0, mListView.getItemIdAtPosition(0));
     }
 
-	protected ListAdapter onCreateAdapter(Context context, Item[] objects) {
-		return new ItemAdapter(context, objects);
-	}
+    protected ListAdapter onCreateAdapter(Context context, Item[] objects) {
+        return new ItemAdapter(context, objects);
+    }
 
-	@Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
-	@Override
-	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		final String fileName = ((Item)parent.getItemAtPosition(position)).getFileName();
-		mTitleView.setText(fileName);
-	}
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        final String fileName = ((Item) parent.getItemAtPosition(position)).getFileName();
+        mTitleView.setText(fileName);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -86,34 +87,34 @@ public class RecorderActivity extends ActionBarActivity implements AdapterView.O
         return super.onOptionsItemSelected(item);
     }
 
-	public void onRecord(View v) {
-		if(!AudioUtils.isMicrophoneAvailable(this)) {
-			Toast.makeText(this, R.string.error_microphone_not_available, Toast.LENGTH_LONG).show();
-			return;
-		}
+    public void onRecord(View v) {
+        if (!AudioUtils.isMicrophoneAvailable(this)) {
+            Toast.makeText(this, R.string.error_microphone_not_available, Toast.LENGTH_LONG).show();
+            return;
+        }
 
-		final String fileName = getSelectedItem().getFileName();
-		if(fileName == null) {
-			Toast.makeText(this, R.string.error_filename_invalid, Toast.LENGTH_LONG).show();
-			return;
-		}
+        final String fileName = getSelectedItem().getFileName();
+        if (fileName == null) {
+            Toast.makeText(this, R.string.error_filename_invalid, Toast.LENGTH_LONG).show();
+            return;
+        }
 
-		AudioRecorderFragment.newInstance(generateExternalStorageFileUri(fileName))
-				.show(getSupportFragmentManager(), AudioRecorderFragment.TAG);
-	}
+        AudioRecorderFragment.newInstance(generateExternalStorageFileUri(fileName))
+                             .show(getSupportFragmentManager(), AudioRecorderFragment.TAG);
+    }
 
-	protected Uri generateExternalStorageFileUri(String fileName) {
-		final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
-		return Uri.fromFile(file);
-	}
+    protected Uri generateExternalStorageFileUri(String fileName) {
+        final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
+        return Uri.fromFile(file);
+    }
 
-	protected Item getSelectedItem() {
-		return (Item)mListView.getItemAtPosition(mListView.getCheckedItemPosition());
-	}
+    protected Item getSelectedItem() {
+        return (Item) mListView.getItemAtPosition(mListView.getCheckedItemPosition());
+    }
 
-	protected class ItemAdapter extends ArrayAdapter<Item> {
-		public ItemAdapter(Context context, Item[] objects) {
-			super(context, R.layout.audio_player_list_item, R.id.item_title, objects);
-		}
-	}
+    protected class ItemAdapter extends ArrayAdapter<Item> {
+        public ItemAdapter(Context context, Item[] objects) {
+            super(context, R.layout.audio_player_list_item, R.id.item_title, objects);
+        }
+    }
 }
